deaccd512348f048625165d183440bf2ca36c270
KenTra222 <Kentraviousc@gmail.com>
2022-07-22 05:03:36 +0000

    added html,css & js files

---
+++ main.js
@@ -0,0 +1,6 @@
+const drop = document.querySelector(".drop")
+const input = document.querySelector(".drop input")
+const text = document.querySelector(".text")
+const progress = document.querySelector(".progress")
+
+let files = []
